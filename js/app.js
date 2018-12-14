a9eced36f91058fa9b93c8de241bfc284523bf4b
Oliver Ristow <ocristow@gmail.com>
2018-12-14 05:12:18 -0700

    commit #1 verbal answers

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// 1. Verbal Questions
+// Arguments and parameters are different names for the same thing, and are also called inputs. Though not required
+// when declaring a function, they are defined when the function is invoked.
+
+// console.log will just print the result of your function into the console so that you can verify its working. 
+// Return may or may not return a value to the programmer, but it also affects the control flow of the code, while 
+// console.log doesn't.
+
+// 2.
